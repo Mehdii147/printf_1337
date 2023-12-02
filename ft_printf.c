44631257d499011f74aeae00ad971683dd1d8797
Elmehdi Hafiane <ehafiane@e2r4p4.1337.ma>
2023-12-02 12:25:53 +0100

    printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ehafiane <ehafiane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 18:38:37 by ehafiane          #+#    #+#             */
-/*   Updated: 2023/11/29 23:24:19 by ehafiane         ###   ########.fr       */
+/*   Updated: 2023/12/02 12:23:14 by ehafiane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,19 @@ int	ft_printf(const char *s, ...)
 	va_start(p, s);
 	size = 0;
 	i = 0;
-	if (write(1, " ", 0))
+	if (write(1, "", 0))
 		return (-1);
 	while (s[i])
 	{
-		if (s[i] == '%' && s[i + 1])
+		if (s[i] == '%')
 		{
 			size += ft_check(s[i + 1], p);
 			i++;
 		}
 		else 
 			size += ft_putchar(s[i]);
-		i++;
+		if (s[i])
+			i++;
 	}
 	va_end(p);
 	return (size);
